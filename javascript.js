12f28161b74db0810de80fd8a0878fc1d29e83d9
Ruben Abel <rubenlage12@gmail.com>
2021-10-06 23:24:50 +0200

    combate pokemooooon

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 let aleatorio = Math.floor(Math.random() * (151 - 0) +1)
-
-
+let aleatorio2 = Math.floor(Math.random() * (151 - 0) +1)
 
 const pokemonRandom = async (name) => {
     try {
@@ -19,12 +18,37 @@ const pokemonRandom = async (name) => {
     }
 }
 
-
-
-
 pokemonRandom("")
-    .then(data => console.log(data))
+    .then(data =>{
+        console.log(data);
+        document.getElementById("pokenomb").innerHTML = data[0];
+        document.getElementById("pokefoto").src = data[1]
+    }
+)
 
 
 
-// document.getElementById("imprimir").innerHTMl
+const pokemonRandom2 = async (name) => {
+    try {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}?limit=1118`);
+        let data = await response.json();
+        let nombres =  data.results[aleatorio2].name;
+        let url =  data.results[aleatorio2].url
+        let foto = await fetch(`${url}`)
+        let convertir = await foto.json()
+        let loconvertido = convertir.sprites.front_default
+        return [nombres, loconvertido];
+    }
+
+    catch (error) {
+        console.log(`ERROR: ${error.stack}`)
+    }
+}
+
+pokemonRandom2("")
+    .then(data =>{
+        console.log(data);
+        document.getElementById("pokenomb2").innerHTML = data[0];
+        document.getElementById("pokefoto2").src = data[1]
+    }
+)
